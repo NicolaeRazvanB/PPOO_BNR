11e72b6ccf270c359cdca0c3f3541f9656c1266f
NicolaeRazvanB <nicolassky16@gmail.com>
2022-11-11 21:50:45 +0200

    Added saving mechanism

--- src/Main.java
+++ src/Main.java
@@ -16,25 +16,71 @@ public class Main {
         String optiune;
         Client clientActiv = null;
         while (menuSwitch) {
-            System.out.println("Bine ati venit la Banca Transilvenia");
-            optiune = scan.nextLine().trim();
-            if (optiune.equals("1")) {
-                optiuni.Inregistrare(scan, listaClienti);
-            }
-            if (optiune.equals("2")) {
-                System.out.println("Meniu autentificare:\n");
-                String nume, parola = "";
-                System.out.println("Introduceti nume");
-                nume = scan.nextLine();
-                System.out.println("Introduceti parola");
-                parola = scan.nextLine();
-                clientActiv = new Client(optiuni.Autentificare(nume, parola, listaClienti));
-            }
-            if (optiune.equals("0")) {
-                optiuni.SalvareDateFisier(listaClienti);
-                menuSwitch = false;
-            }
+            if (clientActiv == null) {
+                System.out.println("Bine ati venit la Banca Transilvenia");
+                System.out.println(
+                        "1.Client nou\n" +
+                                "2.Autentificare\n" +
+                                "3.Inchidere aplicatie\n");
+                optiune = scan.nextLine().trim();
+                if (optiune.equals("1")) {
+                    optiuni.Inregistrare(scan, listaClienti);
+                }
+                if (optiune.equals("2")) {
+                    System.out.println("Meniu autentificare:\n");
+                    String nume, parola = "";
+                    System.out.println("Introduceti nume");
+                    nume = scan.nextLine();
+                    System.out.println("Introduceti parola");
+                    parola = scan.nextLine();
+                    clientActiv = new Client(optiuni.Autentificare(nume, parola, listaClienti));
+                }
+                if (optiune.equals("3")) {
+                    optiuni.SalvareDateFisier(listaClienti);
+                    menuSwitch = false;
+                }
+            } else {
+                System.out.println("Salut " + clientActiv.getNume() + "\n");
+                System.out.println(
+                        "1.Verifica sold carduri\n" +
+                                "2.Extrage/Depune Bani\n" +
+                                "3.Descarca extras conturi\n" +
+                                "4.Iesire cont\n" +
+                                "5.Adaugare card nou\n" +
+                                "6.Modificare PIN\n" +
+                                "7.Inchidere aplicatie\n");
+                optiune = scan.nextLine().trim();
 
+                if (optiune.equals("1")) {
+                    optiuni.ArataSolduri(clientActiv, listaClienti);
+
+                }
+                if (optiune.equals("2")) {
+                    optiuni.Tranzactioneaza(clientActiv, scan, listaClienti);
+
+                }
+
+                if (optiune.equals("3")) {
+                    optiuni.DescarcaExtrasFisier(clientActiv, listaClienti);
+
+                }
+                if (optiune.equals("4")) {
+                    clientActiv = null;
+
+                }
+                if (optiune.equals("5")) {
+                    optiuni.CreeazaCard(clientActiv, listaClienti);
+                }
+                if (optiune.equals("6")) {
+                    optiuni.ModificaPIN(clientActiv, scan, listaClienti);
+                }
+
+                if (optiune.equals("7")) {
+                    optiuni.SalvareDateFisier(listaClienti);
+                    menuSwitch = false;
+                }
+
+            }
 
         }
 
--- src/entitati/Optiuni.java
+++ src/entitati/Optiuni.java
@@ -1,5 +1,7 @@
 package entitati;
 
+import com.sun.jdi.IntegerValue;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
@@ -32,7 +34,6 @@ public class Optiuni {
     }
 
     public void SalvareDateFisier(ClientCollection listaClienti) throws IOException {
-        //Salvare date la inchidere
         FileWriter actualizatorDate = new FileWriter("date.txt");
         actualizatorDate.write(listaClienti.toString());
         actualizatorDate.close();
@@ -49,4 +50,91 @@ public class Optiuni {
         return match;
     }
 
+    public void ArataSolduri(Client client, ClientCollection listaClienti) {
+        for (Client clientIterat : listaClienti.getListaClienti()
+        ) {
+            if (clientIterat.getNume().equals(client.getNume())) {
+                for (Card card : clientIterat.getListaCarduri()) {
+                    System.out.println("IBAN: " + card.getIBAN());
+                    System.out.println("Sold: " + card.getSold() + "\n");
+                }
+            }
+        }
+
+    }
+
+    public void DescarcaExtrasFisier(Client client, ClientCollection listaClienti) throws IOException {
+        for (Client clientIterat : listaClienti.getListaClienti()
+        ) {
+            if (clientIterat.getNume().equals(client.getNume())) {
+                FileWriter descarcatorExtrase = new FileWriter("extrase.txt");
+                descarcatorExtrase.write(clientIterat.getListaCarduri().toString().substring(1, clientIterat.getListaCarduri().toString().length() - 1));
+                descarcatorExtrase.close();
+                System.out.println("Extras de cont descarcat cu succes");
+            }
+        }
+
+    }
+
+    public void CreeazaCard(Client client, ClientCollection listaClienti) {
+        Card card = new Card();
+        for (Client clientIterat : listaClienti.getListaClienti()
+        ) {
+            if (clientIterat.getNume().equals(client.getNume())) {
+                clientIterat.getListaCarduri().add(card);
+                System.out.println("Card adaugat cu succes");
+            }
+        }
+
+    }
+
+    public void ModificaPIN(Client client, Scanner scan, ClientCollection listaClienti) {
+        System.out.println("Alegeti cardul");
+        int i = 1;
+
+        for (Client clientIterat : listaClienti.getListaClienti()
+        ) {
+
+            if (clientIterat.getNume().equals(client.getNume())) {
+                for (Card card : clientIterat.getListaCarduri()) {
+                    System.out.println(i + " .IBAN: " + card.getIBAN() + "Sold: " + card.getSold() + "\n");
+                    i++;
+
+                }
+                int opt = Integer.parseInt(scan.nextLine().trim());
+                System.out.println("Introduceti noul PIN");
+                String PIN = scan.nextLine();
+                clientIterat.getListaCarduri().get(opt - 1).setPIN(PIN);
+                System.out.println("PIN schimbat cu succes");
+
+            }
+        }
+    }
+
+    public void Tranzactioneaza(Client client, Scanner scan, ClientCollection listaClienti) throws InterruptedException {
+        System.out.println("Alegeti cardul");
+        int i = 1;
+
+        for (Client clientIterat : listaClienti.getListaClienti()
+        ) {
+
+            if (clientIterat.getNume().equals(client.getNume())) {
+                for (Card card : clientIterat.getListaCarduri()) {
+                    System.out.println(i + " .IBAN: " + card.getIBAN() + "Sold: " + card.getSold() + "\n");
+                    i++;
+
+                }
+                int opt = Integer.parseInt(scan.nextLine().trim());
+                System.out.println("Introduceti valoarea");
+                String valoare = scan.nextLine();
+                clientIterat.getListaCarduri().get(opt - 1).realizeazaTranzactie(Float.parseFloat(valoare));
+                System.out.println(clientIterat);
+                System.out.println("Tranzactie realizata cu succes");
+
+            }
+        }
+
+    }
 }
+
+
