0d251d076de6628e1cbd8e40223beedc056a9ea2
NicolaeRazvanB <nicolassky16@gmail.com>
2022-11-11 16:51:26 +0200

    Added saving mechanism

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import entitati.ClientCollection;
 import entitati.ExtractorDateText;
 
+import java.io.FileWriter;
+
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         ClientCollection listaClienti = new ExtractorDateText().extrageDate();
-        System.out.println(listaClienti);
 
+
+        //Salvare date la inchidere
+        FileWriter actualizatorDate = new FileWriter("date.txt");
+        actualizatorDate.write(listaClienti.toString());
+        actualizatorDate.close();
+        System.out.println("Fisiere de date actualizate cu succes");
     }
 }
